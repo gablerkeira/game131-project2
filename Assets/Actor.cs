e2d037b004008b8dce1bd95e647d67cf0b8a5afb
svc-student <svc-student@student.directory.jccc>
2019-04-09 19:20:58 -0500

    Hopefully the whole project

---
+++ Assets/Actor.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Actor : MonoBehaviour {
+
+    #region Enums
+
+    public enum TargetSelectionRule
+    {
+        AnyAvailable,
+        HighestHealth,
+        StrongestAttack
+    }
+    public TargetSelectionRule targetSelectionRule;
+
+    [Tooltip("Don't change this property; it should be read-only.")]
+    public Actor currentTarget;
+
+    public enum ActionTarget
+    {
+        MeleeEnemy,
+        AnyEnemy,
+        AnyAlly,
+        AllEnemy,
+        AllAlly
+    }
+
+    public enum ActionEffect
+    {
+        Normal,
+        Disable,
+        Heal
+    }
+
+    public enum ActionSource
+    {
+        Weapon,
+        Life,
+        Death,
+        Fire,
+        Earth,
+        Water,
+        Air
+    }
+
+    public enum Position
+    {
+        left_rear_top,
+        left_rear_center,
+        left_rear_bottom,
+        left_front_top,
+        left_front_center,
+        left_front_bottom,
+        right_front_top,
+        right_front_center,
+        right_front_bottom,
+        right_rear_top,
+        right_rear_center,
+        right_rear_bottom
+    }
+
+    #endregion
+
+    #region Configurable member variables
+
+    public string actorName;
+
+    public int maxHitPoints = 100;
+    public int hitPoints = 100;
+
+    public int initiative = 50;
+
+    public ActionTarget actionTarget;
+
+    public int damage = 25;
+
+    public ActionEffect actionEffect;
+
+    public ActionSource actionEffectSource;
+
+    public ActionSource[] immunities;
+
+    public int percentChanceToHit = 75;
+
+    public Position boardPosition;
+
+    // TODO: Provide some means of configuring how the target is selected
+
+    #endregion
+
+    #region Private member variables
+
+    private BoardData boardData;
+
+    #endregion
+
+    #region Unity events (do not change)
+
+    void Start()
+    {
+        boardData = GameObject.FindGameObjectWithTag("Board").GetComponent<BoardData>();
+    }
+
+    void Update()
+    {
+        currentTarget = RefreshTargetSelection(GetAvailableTargets());
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Modify this function however you need to. The main challenge of this project
+    /// is to create a form-based means of authoring the AI for each actor based on
+    /// the targets they can currently reach. This function receives a list of 
+    /// available targets, and should choose an appropriate target based on how the
+    /// designer has configured the AI.
+    /// 
+    /// How you implement this is up to you. Refer to the project requirements for
+    /// what it has to be able to do.
+    /// </summary>
+    Actor RefreshTargetSelection(List<Actor> availableTargets)
+    {
+        if (availableTargets.Count == 0)
+        {
+            return null;
+        }
+        switch (targetSelectionRule)
+        {
+            case TargetSelectionRule.AnyAvailable:
+                return availableTargets[Random.Range(0, availableTargets.Count)];
+            case TargetSelectionRule.HighestHealth:
+                int highestHealth = 0;
+                for (int i = 0; i < availableTargets.Count; i++)
+                    if (availableTargets[i].hitPoints > highestHealth)
+                        highestHealth = availableTargets[i].hitPoints;
+                List<int> highestHealthIndexes = new List<int>();
+                for (int i = 0; i < availableTargets.Count; i++)
+                    if (availableTargets[i].hitPoints == highestHealth)
+                        highestHealthIndexes.Add(i);
+                return availableTargets[highestHealthIndexes[Random.Range(0, highestHealthIndexes.Count)]];
+            case TargetSelectionRule.StrongestAttack:
+                int highestAttack = 0;
+                for (int i = 0; i < availableTargets.Count; i++)
+                    if (availableTargets[i].damage > highestAttack)
+                        highestAttack = availableTargets[i].damage;
+                List<int> highestAttackIndexes = new List<int>();
+                for (int i = 0; i < availableTargets.Count; i++)
+                    if (availableTargets[i].damage == highestAttack)
+                        highestAttackIndexes.Add(i);
+                return availableTargets[highestAttackIndexes[Random.Range(0, highestAttackIndexes.Count)]];
+        }
+        return availableTargets[Random.Range(0, availableTargets.Count)];
+    }
+
+    #region Target selection core (do not change)
+
+    BoardData.Side MySide { get { return (BoardData.Side)System.Enum.Parse(typeof(BoardData.Side), boardPosition.ToString().Split('_')[0]); } }
+    BoardData.Rank MyRank { get { return (BoardData.Rank)System.Enum.Parse(typeof(BoardData.Rank), boardPosition.ToString().Split('_')[1]); } }
+    BoardData.Line MyLine { get { return (BoardData.Line)System.Enum.Parse(typeof(BoardData.Line), boardPosition.ToString().Split('_')[2]); } }
+
+    List<Actor> GetAvailableTargets()
+    {
+        List<Actor> result = new List<Actor>();
+        BoardData.Side enemySide = MySide == BoardData.Side.left ? BoardData.Side.right : BoardData.Side.left;
+
+        BoardData.Rank[] rankTargetOrder = new BoardData.Rank[] { BoardData.Rank.front, BoardData.Rank.rear };
+
+        if (actionTarget == ActionTarget.MeleeEnemy)
+        {
+            // The weird one.
+            // If I'm in the back row and anybody is in front of me, I cannot attack.
+            if (MyRank == BoardData.Rank.rear)
+            {
+                if (boardData.GetActorByPosition(MySide, BoardData.Rank.front, BoardData.Line.top) != null
+                    || boardData.GetActorByPosition(MySide, BoardData.Rank.front, BoardData.Line.center) != null
+                    || boardData.GetActorByPosition(MySide, BoardData.Rank.front, BoardData.Line.bottom) != null)
+                {
+                    return result;
+                }
+            }
+
+            // Melee units can only attack units that are right in front of them, or one line away from
+            // their current line. They can only attack the rear rank once the front rank is empty.
+            for (int i = 0; i < rankTargetOrder.Length && result.Count == 0; i++)
+            {
+                BoardData.Rank targetRank = rankTargetOrder[i];
+
+                // I can always hit the center...
+                Actor candidate = boardData.GetActorByPosition(enemySide, targetRank, BoardData.Line.center);
+                if (candidate != null)
+                {
+                    result.Add(candidate);
+                }
+                // ... and my own line (applicable only if I'm not at the center).
+                if (MyLine != BoardData.Line.center)
+                {
+                    candidate = boardData.GetActorByPosition(enemySide, targetRank, MyLine);
+                    if (candidate != null) result.Add(candidate);
+                }
+
+
+                // I can only hit across the field if there's nobody in the way (applies to bottom and top lines only).
+                if (MyLine == BoardData.Line.center || (MyLine == BoardData.Line.top && result.Count == 0))
+                {
+                    candidate = boardData.GetActorByPosition(enemySide, targetRank, BoardData.Line.bottom);
+                    if (candidate != null) result.Add(candidate);
+                }
+                if (MyLine == BoardData.Line.center || (MyLine == BoardData.Line.bottom && result.Count == 0))
+                {
+                    candidate = boardData.GetActorByPosition(enemySide, targetRank, BoardData.Line.top);
+                    if (candidate != null) result.Add(candidate);
+                }
+            }
+
+        }
+        else
+        {
+            BoardData.Line[] lines = new BoardData.Line[] { BoardData.Line.top, BoardData.Line.bottom, BoardData.Line.center };
+            BoardData.Side targetSide = (actionTarget.ToString().EndsWith("Enemy")) ? enemySide : MySide;
+            for (int l = 0; l < lines.Length; l++)
+                for (int r = 0; r < rankTargetOrder.Length; r++)
+                {
+                    Actor candidate = boardData.GetActorByPosition(targetSide, rankTargetOrder[r], lines[l]);
+                    if (candidate != null) result.Add(candidate);
+                }
+
+        }
+        return result;
+    }
+
+    #endregion
+
+
+}
---
+++ Assets/Editor/ActorCustomEditor.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(Actor))]
+public class ActorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Actor myActor = target as Actor;
+
+        #region Actor Name
+        myActor.actorName = EditorGUILayout.TextField("Actor Name: ", myActor.actorName);
+        #endregion
+
+        #region Action Target
+        myActor.actionTarget = (Actor.ActionTarget)EditorGUILayout.EnumPopup("Action Target: ", myActor.actionTarget);
+
+        #endregion
+
+        #region Action Effect
+        myActor.actionEffect = (Actor.ActionEffect)EditorGUILayout.EnumPopup("Action Effect: ", myActor.actionEffect);
+        #endregion
+
+        #region AES & Immunities
+        Actor.ActionSource[] sourceValues = Enum.GetValues(typeof(Actor.ActionSource)) as Actor.ActionSource[];
+        string[] sourceNames = Enum.GetNames(typeof(Actor.ActionSource));
+
+        SelectionList<Actor.ActionSource> sources = new SelectionList<Actor.ActionSource>(sourceValues, sourceNames);
+        myActor.actionEffectSource = sources.RadioList("Action Source: ", myActor.actionEffectSource, 3);
+        myActor.immunities = sources.CheckboxList("Immunities: ", myActor.immunities, 3);
+        #endregion
+
+        #region MaxHit Points
+        string mhpLabel = "Max Hit Points: "; 
+        int newMaxHitPoints = myActor.GetComponent<Actor>().maxHitPoints;
+        newMaxHitPoints = EditorGUILayout.IntSlider(mhpLabel, newMaxHitPoints, 0, 1500);
+        myActor.GetComponent<Actor>().maxHitPoints = newMaxHitPoints;
+        #endregion
+
+        #region Hit Points
+        string hpLabel = "Hit Points: ";
+        int newHitPoints = myActor.GetComponent<Actor>().hitPoints;
+        newHitPoints = EditorGUILayout.IntSlider(hpLabel, newHitPoints, 1, 100);
+        myActor.GetComponent<Actor>().hitPoints = newHitPoints;
+        #endregion
+
+        #region Initiative
+        string iLabel = "Initiative: ";
+        int newInitiative = myActor.GetComponent<Actor>().initiative;
+        newInitiative = EditorGUILayout.IntSlider(iLabel, newInitiative, 10, 100);
+        myActor.GetComponent<Actor>().initiative = newInitiative;
+
+        #endregion
+
+        #region Damage
+        string dLabel = "Damage: ";
+        int newDamage = myActor.GetComponent<Actor>().damage;
+        newDamage = EditorGUILayout.IntSlider(dLabel, newDamage, 0, 180);
+        myActor.GetComponent<Actor>().damage = newDamage;
+        #endregion
+
+        #region Percent Chance to Hit
+        string pchLabel = "Percent Chance To Hit:  ";
+        int newPcth = myActor.GetComponent<Actor>().percentChanceToHit;
+        newPcth = EditorGUILayout.IntSlider(pchLabel, newPcth, 0, 100);
+        myActor.GetComponent<Actor>().percentChanceToHit = newPcth;
+        #endregion
+
+        #region Board Position
+        Actor.Position[] positionValues = Enum.GetValues(typeof(Actor.Position)) as Actor.Position[];
+        string[] positionNames = Enum.GetNames(typeof(Actor.Position));
+        SelectionList<Actor.Position> positions = new SelectionList<Actor.Position>(positionValues, positionNames);
+        myActor.boardPosition = positions.PositionGrid("Board Position: ", myActor.boardPosition, 3);
+        #endregion
+    }
+
+}
+
+class SelectionList<T> where T : IComparable
+{
+    T[] _values;
+    string[] _labels;
+    T _selectedValue;
+
+    #region Checkbox List
+    public T[] CheckboxList(string label, T[] initialSelections, int itemsPerRow)
+    {
+        List<T> selectedValues = new List<T>();
+        List<int> initialSelectedIndexes = new List<int>();
+        for (int i = 0; i < _values.Length; i++)
+        {
+            for (int j = 0; j < initialSelections.Length; j++)
+            {
+                if (_values[i].CompareTo(initialSelections[j]) == 0) initialSelectedIndexes.Add(i);
+            }
+        }
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField(label, GUILayout.MaxWidth(100));
+
+        EditorGUILayout.BeginHorizontal();
+        for (int r = 0; r < _values.Length; r += itemsPerRow)
+        {
+            EditorGUILayout.BeginVertical();
+            for (int i = r; i < r + itemsPerRow && i < _values.Length; i++)
+            {
+                if (GUILayout.Toggle(initialSelectedIndexes.Contains(i), _labels[i]))
+                {
+                    selectedValues.Add(_values[i]);
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+
+        return selectedValues.ToArray();
+
+    }
+    #endregion
+
+    #region RadioList
+    public T RadioList(string label, T initialSelection, int itemsPerRow)
+    {
+        T originalSelectedValue = _selectedValue;
+        _selectedValue = initialSelection;
+        bool anyChecked = false;
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField(label, GUILayout.MaxWidth(100));
+
+        EditorGUILayout.BeginHorizontal();
+        for (int r = 0; r < _values.Length; r += itemsPerRow)
+        {
+            EditorGUILayout.BeginVertical();
+            for (int i = r; i < r + itemsPerRow && i < _values.Length; i++)
+            {
+                if (_values[i].CompareTo(initialSelection) == 0) originalSelectedValue = initialSelection;
+                if (GUILayout.Toggle(_values[i].CompareTo(_selectedValue) == 0, _labels[i]))
+                {
+                    _selectedValue = _values[i];
+                    anyChecked = true;
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+
+        if (!anyChecked) _selectedValue = originalSelectedValue;
+        return _selectedValue;
+    }
+    #endregion
+
+    #region SelectionList
+    public SelectionList(T[] values, string[] labels)
+    {
+        _values = new T[values.Length];
+        _labels = new string[labels.Length < values.Length ? values.Length : labels.Length];
+        for (int i = 0; i < _values.Length; i++) _values[i] = values[i];
+        for (int i = 0; i < _labels.Length; i++) _labels[i] = (i < labels.Length) ? labels[i] : values[i].ToString();
+        _selectedValue = _values[0];
+    }
+    #endregion
+
+    #region PositionGrid
+    public T PositionGrid(string label, T initialSelection, int itemsPerRow)
+    {
+        T originalSelectedValue = _selectedValue;
+        _selectedValue = initialSelection;
+        bool anyChecked = false;
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField(label, GUILayout.MaxWidth(100));
+
+        EditorGUILayout.BeginHorizontal();
+        for (int r = 0; r < _values.Length; r += itemsPerRow)
+        {
+            EditorGUILayout.BeginVertical();
+            for (int i = r; i < r + itemsPerRow && i < _values.Length; i++)
+            {
+                if (_values[i].CompareTo(initialSelection) == 0) originalSelectedValue = initialSelection;
+                if (GUILayout.Toggle(_values[i].CompareTo(_selectedValue) == 0, _labels[i]))
+                {
+                    _selectedValue = _values[i];
+                    anyChecked = true;
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+
+        if (!anyChecked) _selectedValue = originalSelectedValue;
+        return _selectedValue;
+    }
+    #endregion
+}
